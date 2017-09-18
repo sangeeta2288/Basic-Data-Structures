981ac62f73aafc288ef6e20ddf74725308f30869
sangeeta2288 <ashrit.sangeeta@gmail.com>
2017-09-18 13:00:21 -0400

    Paliandrome check-init

---
+++ Queue/strPaliandrome.py
@@ -0,0 +1,26 @@
+#space - O(2n) ==> O(n) extra space
+# Time- O(2n)
+
+str2= ''
+str1= 'manu'
+s=[]
+if (len(str1)>0):
+	for i in range(len(str1)):
+		s.append(str1[i])
+
+while(len(s)>0):
+	str2=str2+s.pop(-1)
+
+if (str1 == str2):
+	print("string-",str1, "is a paliandrome")
+else:
+	print("Given string is not a paliandrome-",str1, str2)	
+
+
+
+
+
+# Leet Code Solution
+#class Solution(object):
+ #   def isPalindrome(self, x):
+ #       return str(x) == str(x)[::-1]	

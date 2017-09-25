d23b5fa84fc7a50006d34c0fec0e2b14278b4b90
sangeeta2288 <ashrit.sangeeta@gmail.com>
2017-09-25 12:51:22 -0400

    init

---
+++ string/Xvalue-inpolynomial.py
@@ -0,0 +1,6 @@
+# Solve for X, eg:x+4 =9; x = 5
+
+# 1.First lets workout solution for exact x + int = int format of the problem
+# equation will be input from user in string format
+# Time-
+# Space-O(1)

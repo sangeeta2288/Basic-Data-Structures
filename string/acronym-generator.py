b3444656ad69f5682397f38f4c2b13ec26437d5e
sangeeta2288 <ashrit.sangeeta@gmail.com>
2017-09-25 12:47:25 -0400

    init

---
+++ string/acronym-generator.py
@@ -0,0 +1,20 @@
+# Eg: Convert given string into an acronym
+#input:Java Virtual Machine=> o/p:JVM
+# Time: O(2n) bcoz of list entry + looping through words===> O(n) where n is no of words
+# Space: O(n) extra space bcoz of the list
+class StrFunction():
+        def __init__(self):
+                #print("Hello")
+
+        def strOperation(self,str):
+                str1=str.upper()
+                print(str1)
+
+                #str=>List
+                list1=str1.split()
+                for word in list1:
+                        print(word[0],end="")
+
+                #get 1st letter of the word and eleminate new line
+
+
